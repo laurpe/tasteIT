9d7409c8d18665a997986f86dba68d063b17c3d3
laurpe <laura.perttila@edu.bc.fi>
2022-05-16 16:40:47 +0300

    base for search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useState, useEffect } from "react";
-
-import axios from "axios";
 
 import Home from "./components/Home";
 import Recipes from "./components/Recipes";
@@ -9,30 +6,11 @@ import AddRecipe from "./components/AddRecipe";
 import RecipeView from "./components/RecipeView";
 
 const App = () => {
-    const [recipes, setRecipes] = useState([]);
-
-    useEffect(() => {
-        const getRecipes = async () => {
-            try {
-                const response = await axios.get(
-                    "http://localhost:3010/recipes"
-                );
-                setRecipes(response.data);
-            } catch (error) {
-                console.error(error.message);
-            }
-        };
-        getRecipes();
-    }, []);
-
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route
-                    path="/recipes"
-                    element={<Recipes recipes={recipes} />}
-                />
+                <Route path="/recipes" element={<Recipes />} />
                 <Route path="/recipes/:id" element={<RecipeView />} />
                 <Route path="/add_recipe" element={<AddRecipe />} />
             </Routes>
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -7,6 +7,8 @@ import axios from "axios";
 
 const Recipes = () => {
     const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState("");
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         const getRecipes = async () => {
@@ -22,9 +24,27 @@ const Recipes = () => {
         getRecipes();
     }, []);
 
+    const handleChange = (event) => {
+        setSearch(event.target.value);
+        const results = recipes.filter((recipe) => {
+            return recipe.name.includes(search);
+        });
+        setResults(results);
+        console.log(results);
+    };
+
     return (
         <>
             <Header />
+            <div>
+                <label htmlFor="search">Search recipe by name</label>
+                <input
+                    type="text"
+                    name="search"
+                    value={search}
+                    onChange={handleChange}
+                />
+            </div>
             <div className="recipes">
                 {recipes.map((recipe) => {
                     return <Recipe key={recipe.id} recipe={recipe} />;
